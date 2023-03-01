ec7186ee8fa06801ecc6966ef9a0838f275900d2
Egor <bubiryov@icloud.com>
2023-03-01 13:10:47 +0200

    Initial Commit

---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Egor Bubiryov on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
